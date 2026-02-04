81b509140ee7c734fc64aa12331740d97a651801
VWNVH <148910959+VWNVH@users.noreply.github.com>
2026-02-04 07:38:50 +0000

    MT19937.py

---
+++ GENERATORS/PRNG_non-cryptographics/MT19937.py
@@ -0,0 +1,64 @@
+# generators/mt19937.py
+"""
+Mersenne Twister MT19937
+PRNG de période 2^19937 -1
+Non cryptographiquement sécurisé
+https://fr.wikipedia.org/wiki/Mersenne_Twister
+https://en.wikipedia.org/wiki/Mersenne_Twister
+"""
+
+W, N, M, R = 32, 624, 397, 31
+A = 0x9908B0DF
+F = 1812433253
+U, D = 11, 0xFFFFFFFF
+S, B = 7, 0x9D2C5680
+T, C = 15, 0xEFC60000
+L = 18
+
+
+def init(seed):
+    state = [seed & 0xFFFFFFFF]
+    for i in range(1, N):
+        prev = state[i - 1]
+        state.append((F * (prev ^ (prev >> 30)) + i) & 0xFFFFFFFF)
+    return state
+
+
+def twist(state):
+    for i in range(N):
+        # Extraire bit 31 de state[i] et bits 0-30 de state[i+1]
+        bit_haut = state[i] & 0x80000000  # 1 bit MSB
+        bits_bas = state[(i + 1) % N] & 0x7FFFFFFF  # 31 bits LSB
+        x = bit_haut | bits_bas
+        
+        # Shift et conditionnelle
+        x_shifted = x >> 1
+        if x % 2 == 1:  # Bit de poids faible = 1
+            x_shifted ^= A
+        
+        state[i] = state[(i + M) % N] ^ x_shifted
+    return state
+
+
+def temper(y):
+    y ^= (y >> U) & D
+    y ^= (y << S) & B
+    y ^= (y << T) & C
+    y ^= y >> L
+    return y & 0xFFFFFFFF
+
+
+def generate(seed, n):
+    state = init(seed)
+    index = N
+    results = []
+    
+    for _ in range(n):
+        if index >= N:
+            twist(state)
+            index = 0
+        
+        results.append(temper(state[index]))
+        index += 1
+    
+    return results
